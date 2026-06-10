31f9f30e5632f5e475208d43c0dbd9e317ae95d6
dev <dev@gmail.com>
2026-06-10 11:49:45 +0530

    added logo in favicon

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   devIndicators: false,
+  async rewrites() {
+    return [
+      {
+        source: '/favicon.ico',
+        destination: '/icon.png',
+      },
+    ];
+  },
 };
 
 export default nextConfig;
